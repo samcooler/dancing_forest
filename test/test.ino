70857618e8b99e2e454d74654c55d4ec58e5c035
Sam Cooler <scooler@gmail.com>
2018-03-12 10:21:34 -0500

    more jean work

--- test/test.ino
+++ test/test.ino
@@ -49,13 +49,49 @@ uint32_t Wheel(byte WheelPos) {
     return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+
+void loop0(){
+  sensors_event_t event; 
+  lis.getEvent(&event);
+
+  float scale = 20;
+  float baseline = 1;
+  uint8_t xValue = baseline + scale * abs(event.acceleration.x);
+  uint8_t yValue = baseline + scale * abs(event.acceleration.y);
+  uint8_t zValue = baseline + scale * abs(event.acceleration.z - 9.8);
+
+
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(xValue, yValue, zValue));
+  }
+  strip.show();
+}
+
+
+
+void loop1(){
+  rainbow(200);
+}
+
+
 //int[] before = new int[strip.numPixels()] ;
 //uint8_t N = strip.numPixels();
 //int N = 60;
 int before[N];
 int current[N];
 
-boolean first = true;
 void loop() {
   sensors_event_t event; 
   lis.getEvent(&event);
@@ -67,26 +103,19 @@ void loop() {
   uint8_t zValue = baseline + scale * abs(event.acceleration.z - 9.8);
 
   
-  current[0] = Wheel(xValue + yValue + zValue);
-  current[59] = Wheel(xValue + yValue + zValue); 
+  current[0] = strip.Color(xValue, yValue, zValue);
+  current[59] = strip.Color(xValue, yValue, zValue); 
   for(uint16_t i=1; i<N/2; i++) {
     current[i] = before[i - 1];
     current[(60-i-1)] = before[60-i];
   }
   
   for(uint16_t i=0; i<strip.numPixels(); i++) {
-   
-    
     //Serial.print("\t\tX: "); Serial.print(event.acceleration.x); 
     //Serial.print("\t "); Serial.print(xValue);
     //Serial.print("\t "); Serial.print(current[0]);
     //Serial.print("\t "); Serial.print(current[1]);
     //Serial.print("\n");
-    //if(i == 0){
-    //  strip.setPixelColor(i, strip.Color(255, 0, 0));
-    //}else{
-    //  strip.setPixelColor(i, strip.Color(0, 255, 0));
-    //}
     strip.setPixelColor(i, current[i]);
   }
   for(uint16_t i=0; i<strip.numPixels(); i++) {
