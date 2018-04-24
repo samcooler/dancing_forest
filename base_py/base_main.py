711f1203399d8757e8a4250fba6aece1e82e34d1
Sam Cooler <scooler@gmail.com>
2018-04-24 13:00:49 -0500

    add python base code start

---
+++ base_py/base_main.py
@@ -0,0 +1,21 @@
+# includes
+
+
+# setup
+
+# start serial read thread
+
+# start light pattern threads
+
+
+# main loop start
+
+# collect inputs with history
+
+# detect motion patterns
+
+# update light patterns
+
+# send serial output
+
+# main loop end
